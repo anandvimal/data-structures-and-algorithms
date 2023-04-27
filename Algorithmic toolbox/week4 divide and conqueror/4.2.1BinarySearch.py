92c1012f8689f8b369ea60be45150650b6830814
Deepak Sandhu <deepaksandhu789@gmail.com>
2023-04-26 20:28:37 -0700

    binary search working

---
+++ Algorithmic toolbox/week4 divide and conqueror/4.2.1BinarySearch.py
@@ -0,0 +1,42 @@
+def binary_search(sorted_list: list,lookup_item: int ):
+    low = 0
+    high = len(sorted_list)-1
+    #print(f"looking for {lookup_item}")
+    # implementation
+    while( low <= high ):
+        #print(f"low:{low} high:{high}")
+        mid = int((low+high)/2)
+        if lookup_item == sorted_list[mid]:
+            #print(f"found {sorted_list[mid]}")
+            return mid 
+        elif lookup_item > sorted_list[mid]:
+            low = mid + 1
+        elif lookup_item < sorted_list[mid]:
+            high = mid - 1
+    return -1
+
+
+if __name__ == "__main__":
+    number_of_enteries = int(input()) #ignore
+    original_list = list(map(int, input().split()))
+    number_of_enteries = int(input()) #ignore
+    search_list = list(map(int, input().split()))
+    result_list = ''
+
+    for search_item in search_list:
+        result = binary_search(sorted_list = original_list,lookup_item= search_item )
+        result_list = result_list + str(result) + ' '
+
+    print(result_list)
+
+'''
+binarySearch(arr, x, low, high)
+    repeat till low = high
+           mid = (low + high)/2
+           if (x = arr[mid])
+               return mid
+          else if (x > arr[mid])
+               low = mid + 1
+          else
+               high = mid – 1
+'''
