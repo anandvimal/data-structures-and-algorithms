24c10a7a7306ada8728dcec7b56af4279cbd31c4
Deepak Sandhu <deepaksandhu789@gmail.com>
2023-04-26 22:27:35 -0700

    working solution for duplicate binary soearch

--- Algorithmic toolbox/week4 divide and conqueror/4.2.2BinarySearchWithDuplicates.py
+++ Algorithmic toolbox/week4 divide and conqueror/4.2.2BinarySearchWithDuplicates.py
@@ -1,27 +1,29 @@
+from math import floor
+
+
 def binary_search(sorted_list: list,lookup_item: int ):
     low = 0
-    high = len(sorted_list)-1
+    high = len(sorted_list)
     #print(f"looking for {lookup_item}")
     # implementation
-    while( low <= high ):
+    while( low < high ):
         #print(f"low:{low} high:{high}")
-        mid = int((low+high)/2)
-        if lookup_item == sorted_list[mid]:
-            #print(f"found {sorted_list[mid]}")
-            return mid 
-        elif lookup_item > sorted_list[mid]:
+        mid = int(floor((low+high)/2))
+        if sorted_list[mid] < lookup_item :
             low = mid + 1
-        elif lookup_item < sorted_list[mid]:
-            high = mid - 1
-    return -1
+        else:
+            high = mid
+    if low < 0 or low > len(sorted_list)-1:
+        return -1
+    elif sorted_list[low] == lookup_item:
+        return low
+    else:
+        low = -1
+        #return -1
+    return low
 
 def binary_search_wrapper(sorted_list: list, lookup_item: int):
     result_index = binary_search(sorted_list, lookup_item)
-    while (result_index > 0 and sorted_list[result_index] == lookup_item):
-        if sorted_list[result_index-1] == lookup_item:
-            result_index = result_index-1
-        else:
-            return result_index
     return result_index
     
 
@@ -32,19 +34,20 @@ if __name__ == "__main__":
     search_list = list(map(int, input().split()))
     result_list = ''
     for search_item in search_list:
-        result = binary_search_wrapper(sorted_list = original_list,lookup_item= search_item )
+        result = binary_search(sorted_list = original_list,lookup_item= search_item )
         result_list = result_list + str(result) + ' '
     print(result_list)
 
 
 '''
-binarySearch(arr, x, low, high)
-    repeat till low = high
-           mid = (low + high)/2
-           if (x = arr[mid])
-               return mid
-          else if (x > arr[mid])
-               low = mid + 1
-          else
-               high = mid – 1
+function binary_search_leftmost(A, n, T):
+    L := 0
+    R := n
+    while L < R:
+        m := floor((L + R) / 2)
+        if A[m] < T:
+            L := m + 1
+        else:
+            R := m
+    return L
 '''
