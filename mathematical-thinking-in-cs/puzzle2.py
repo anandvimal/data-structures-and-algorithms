6b3c05ddd73fa144ec08ef10249714d2c03332c1
Deepak Sandhu <deepaksandhu789@gmail.com>
2023-05-03 22:18:51 -0700

    fixed puzzle 2

--- mathematical-thinking-in-cs/puzzle2.py
+++ mathematical-thinking-in-cs/puzzle2.py
@@ -1,14 +1,16 @@
-#find a two digit number that becomes 7 times smaller when first digit is removed.
+#find a two digit number that becomes 57 times smaller when first digit is removed.
 
 test = 10
 found = False
 
-while test<10000:
+while test<10000000 and found == False:
     test_string=str(test)
-    test_string=test_string[1]
+    test_string=test_string[1:]
+    print(f"test_string:{test_string} test:{test}")
     result = int(test_string)
-    if test/7 == result:
-        print("number is: "+str(test)+"after removing first digit it becomes 1/7 : "+ str(result))
+    if test/57 == result:
+        print("number is: "+str(test)+"after removing first digit it becomes 1/57 : "+ str(result))
+        found = True #this will just break the loop
     #print(test)
     #print(test_string)
 
