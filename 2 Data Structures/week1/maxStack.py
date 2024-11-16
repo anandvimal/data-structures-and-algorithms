f7f01cedf8c000aedbb951a6b3c77b19aa5be67a
Deepak Sandhu <deepaksandhu789@gmail.com>
2024-11-16 10:25:14 -0800

    max stack working

--- 2 Data Structures/week1/maxStack.py
+++ 2 Data Structures/week1/maxStack.py
@@ -3,15 +3,25 @@ class Stack:
     def __init__(self):
         self.length = 0
         self.data = []
+        self.max = []
+    
+    def max(self):
+        if len(self.data) > 0:
+            return self.data[0]
     
     def isempty(self):
         if self.length == 0:
             return True
         else:
-            print(self.data)
+            print(f"data: {self.data}")
+            print(f"max : {self.max}")
             return False
                 
-    def push(self, item: str):
+    def push(self, item: int):
+        if self.isempty() or item > self.max[0]:
+            self.max.insert(0, item)
+        else:
+            self.max.insert(0, self.max[0])
         self.data.insert(0, item)
         self.length = self.length+1
         print(self.data)
@@ -21,6 +31,7 @@ class Stack:
         if self.length > 0:
             top = self.data[0]
             self.data.pop(0)
+            self.max.pop(0)
             self.length = self.length-1
             print(self.data)
             return top
