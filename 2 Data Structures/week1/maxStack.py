ff986a49c72d7de3126962dc2222255372a793a7
Deepak Sandhu <deepaksandhu789@gmail.com>
2024-11-16 11:03:20 -0800

    input with grader working max function still have issues

--- 2 Data Structures/week1/maxStack.py
+++ 2 Data Structures/week1/maxStack.py
@@ -3,48 +3,69 @@ class Stack:
     def __init__(self):
         self.length = 0
         self.data = []
-        self.max = []
+        self.maximum = []
     
-    def max(self):
-        if len(self.data) > 0:
-            return self.data[0]
     
     def isempty(self):
         if self.length == 0:
             return True
         else:
-            print(f"data: {self.data}")
-            print(f"max : {self.max}")
+            #print(f"data: {self.data}")
+            #print(f"max : {self.maximum}")
             return False
                 
-    def push(self, item: int):
-        if self.isempty() or item > self.max[0]:
-            self.max.insert(0, item)
+    def push(self, item: str):
+        if self.isempty() or item > self.maximum[0]:
+            self.maximum.insert(0, item)
         else:
-            self.max.insert(0, self.max[0])
+            pass
+            #self.maximum.insert(0, self.maximum[0])
         self.data.insert(0, item)
         self.length = self.length+1
-        print(self.data)
+        #print(self.data)
         pass
 
     def pop(self):
         if self.length > 0:
             top = self.data[0]
             self.data.pop(0)
-            self.max.pop(0)
+            self.maximum.pop(0)
             self.length = self.length-1
-            print(self.data)
+            #print(self.data)
+            print(top)
             return top
         else:
-            return False
+            #return False
+            pass
+        
+    def big(self):
+        #print(f"length: {self.length}")
+        if self.length > 0:
+            top = self.maximum[0]
+            print(top)
+            #return top
 
 
-mystack = Stack()
-print(mystack.isempty())
-mystack.push(1)
-mystack.push(2)
-mystack.push(1)
-mystack.pop()
-print(mystack.isempty())
+# mystack = Stack()
+# print(mystack.isempty())
+# mystack.push(1)
+# mystack.push(2)
+# mystack.push(1)
+# mystack.big()
+# mystack.pop()
+# print(mystack.isempty())
 
 
+if __name__ == "__main__":
+    mystack = Stack()
+    times = int(input())
+    for i in range(0,times):
+        cmd = input()
+        if cmd == "pop":
+            mystack.pop()
+        elif cmd == "max":
+            mystack.big()
+        elif cmd.startswith("push"):
+            cmd = cmd[4:]
+            # print(f"pushing cmd: {cmd}")
+            mystack.push(cmd)
