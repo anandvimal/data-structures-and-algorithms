c107190be946adc95e37dd1789851b8298f50a2b
Deepak Sandhu <deepaksandhu789@gmail.com>
2024-11-23 23:23:21 -0800

    Binary tree practice

--- binary trees/2 implementation of binary tree no null/Tree.py
+++ binary trees/2 implementation of binary tree no null/Tree.py
@@ -30,11 +30,51 @@ def insertNode(root:Node, key:int):
         root.right = insertNode(root.right, key)
     return root
 
+def deleteNode(root:Node, key:int):
+    if isPlaceHolderNode(root):
+        return root
+    if key < root.key:
+        root.left = deleteNode(root.left, key)
+    elif key > root.key:
+        root.right = deleteNode(root.right, key)
+    else: #node found
+        #case: node haas 0/ one right child node
+        if isPlaceHolderNode(root.left):
+            temp:Node = root.right
+            #delete(root)
+            #root = root.right
+            return temp
+        
+        #case: node has one left child node
+        elif isPlaceHolderNode(root.right):
+            temp:Node = root.left
+            #delete(root)
+            #root= root.left
+            return temp
+        
+        temp:Node = minValueNode(root.right)
+
+        root.key = temp.key
+
+        root.right = deleteNode(root.right, temp.key)
+    return root
+
+def minValueNode(root:Node):
+    current:Node = root
+    #search the leftmost tree
+    while isPlaceHolderNode(current) == False and isPlaceHolderNode(current.left):
+        current = current.left
+    return current
+
 root = getNewNode(1)
 print(root.key)
 print(root.left.key)
 print(root.right.key)
 root = insertNode(root, 2)
+print(root.key)
+print(root.left.key)
+print(root.right.key)
+root = deleteNode(root,1)
 print(root.key)
 print(root.left.key)
 print(root.right.key)
