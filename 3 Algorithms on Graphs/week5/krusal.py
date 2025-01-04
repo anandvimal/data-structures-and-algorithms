5bd70bd7310b9f1dc1533168dcc17895e943ed98
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-01-03 16:33:34 -0800

    kursal algorithm needs fix for union find.

--- 3 Algorithms on Graphs/week5/krusal.py
+++ 3 Algorithms on Graphs/week5/krusal.py
@@ -1,3 +1,43 @@
+import math
+infinity = math.inf
+#negative_infinity = -math.inf
+
+graph_details = list(map(int, input().split())) 
+n = graph_details[0] # total vertices
+m = graph_details[1] # total edges
+
+#lets make a adj_list
+adj_list = {}
+edges = []
+
+#keep tracks for visited nodes
+#visited = {}
+distance = {}
+previous = {}
+
+#fill adj_list with all vertices
+for vertex in range(1,n+1):
+    adj_list[vertex] = [] 
+    distance[vertex] = infinity
+    previous[vertex] = None
+
+#populate adj list
+for i in range(0,m):
+    edge = list(map(int, input().split()))
+    node_l = edge[0]
+    node_r = edge[1]
+    weight = edge[2]
+    adj_list[node_l].append( [weight,node_r] )
+    #adj_list[node_r].append(node_l)
+    edges.append([node_l, node_r, weight])
+
+#sort edges:
+edges.sort(key=lambda x: x[2])
+
+# get the vertix to check for connected component:
+starting_node, ending_node =  map(int, input().split())
+
+# disjoin sets:
 parent = []
 rank = []
 
@@ -22,4 +62,21 @@ def union(i,j):
         if rank[i_id] == rank[j_id]:
             rank[j_id] = rank[j_id]+1
 
-   
+
+def krusal(adj_list, edges):
+    for vertex in range(0,n):
+        parent[vertex] = vertex 
+        rank[vertex] = 0
+
+    for vertex in range(0,n):
+        make_set(vertex)
+    
+    X = []
+    for edge in edges:
+        u = edge[0]
+        v = edge[1]
+        if find(u) != find(v):
+            X.append[[u,v]]
+            union(u,v)
+    return X
+
