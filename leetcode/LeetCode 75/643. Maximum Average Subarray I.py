900665116dcb025630235e015ac2d5151544c2ef
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-03-25 16:21:14 -0700

    faster and accepted leetcode75 #643

--- leetcode/LeetCode 75/643. Maximum Average Subarray I.py
+++ leetcode/LeetCode 75/643. Maximum Average Subarray I.py
@@ -20,25 +20,38 @@ k = 4
 
 from typing import List
 
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        highest_average = 0
-        sum = 0
-        average = 0
-        if len(nums) > k:
-            for i in range(len( nums)+1-k):
-                sum = 0
-                average = 0
-                for j in range(k):
-                    sum += nums[j+i]
-                average = sum/k
-                if average > highest_average:
-                    highest_average = average
-            return highest_average
-        else:
-            for i in range(len(nums)):
-                sum += nums[i]
-            return sum/len(nums)    
+        max_sum = current_sum = sum(nums[:k])
+
+        for i in range(k, len(nums)):
+            current_sum = current_sum + nums[i] - nums[i-k] #add right, sub left
+            max_sum = max(current_sum, max_sum)
+
+        return max_sum/ k    
+        
+
+#slower
+# class Solution:
+#     def findMaxAverage(self, nums: List[int], k: int) -> float:
+#         highest_average = 0
+#         sum = 0
+#         average = 0
+#         if len(nums) > k:
+#             for i in range(len( nums)+1-k):
+#                 sum = 0
+#                 average = 0
+#                 for j in range(k):
+#                     sum += nums[j+i]
+#                 average = sum/k
+#                 if average > highest_average:
+#                     highest_average = average
+#             return highest_average
+#         else:
+#             for i in range(len(nums)):
+#                 sum += nums[i]
+#             return sum/len(nums)    
         
 
 sol = Solution()
