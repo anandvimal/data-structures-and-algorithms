dac19561472cb18c4d4f17aa7aac84772869034a
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-03-29 21:36:57 -0700

    Create 1004. Max Consecutive Ones III.py

---
+++ leetcode/LeetCode 75/1004. Max Consecutive Ones III.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left= right= zero_count= maximum_length  =0
+
+        for right in range(len(nums)):
+
+            if nums[right] == 0:
+                zero_count +=1
+
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left +=1
+
+
+            length = right-left + 1
+            maximum_length = max(maximum_length, length)
+
+        return maximum_length
+
+
+        
