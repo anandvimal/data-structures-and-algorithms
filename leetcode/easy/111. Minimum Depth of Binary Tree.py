bb9db46fba0f4f81e80af456801187ad5d1ac49e
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-04-22 21:08:18 -0700

    Update 111. Minimum Depth of Binary Tree.py

--- leetcode/easy/111. Minimum Depth of Binary Tree.py
+++ leetcode/easy/111. Minimum Depth of Binary Tree.py
@@ -6,19 +6,20 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        queue= [(root,1)]
         
-        if not root:
-            return 0
-        
-        while queue:
-            node,depth = queue.pop(0)
-
-            if node.left:
-                queue.append((node.left, depth+1))
-            if node.right:
-                queue.append((node.right, depth+1))
-
-            if not node.left and not node.right:
-                return depth
+        def depth(root):
+            queue= [(root,1)]
             
+            if not root:
+                return 0
+
+            while queue:
+                node, depth = queue.pop(0)
+                if node.left:
+                    queue.append((node.left, depth+1))
+                if node.right:
+                    queue.append((node.right, depth+1))
+                if not node.left and not node.right: # first leaf we encounter
+                    return depth
+
+        return depth(root)
