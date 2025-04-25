9603a233b3d713d7d9944587d7db216120df8b4f
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-04-24 21:37:17 -0700

    Create 501. Find Mode in Binary Search Tree.py

---
+++ leetcode/easy/501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        data = {}
+        def inorder(root):
+            nonlocal data
+            if root:
+                inorder(root.left)
+                data[root.val] = data.get(root.val, 0)+1
+                inorder(root.right)
+        inorder(root)
+        #print(data)
+        max_value = -1
+        for key,value in data.items():
+            if value > max_value:
+                max_value = value
+                max_key = key
+
+        return max_key
