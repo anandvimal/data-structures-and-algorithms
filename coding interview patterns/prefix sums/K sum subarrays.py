b50d4df1d75af35574a74fa4f488b41986204d26
Deepak Sandhu <deepaksandhu789@gmail.com>
2025-05-15 13:04:11 -0700

    Create K sum subarrays.py

---
+++ coding interview patterns/prefix sums/K sum subarrays.py
@@ -0,0 +1,29 @@
+from typing import List
+
+def k_sum_subarrays_optimized(nums: List[int], k: int) -> int:
+    count = 0
+    # Initialize the map with 0 to handle subarrays that sum to 'k' 
+    # from the start of the array
+    prefix_sum_map = {0: 0}
+    curr_prefix_sum = 0
+    for num in nums:
+        # Update the current prefix sum, by adding the current number
+        curr_prefix_sum += num
+        # If a subarray with sum 'k' is found, increment the count by the 
+        # number of times that prefix sum has been seen found
+        print(f"count : {count}  curr_prefix_sum: {curr_prefix_sum}  num: {num}")
+        if curr_prefix_sum - k in prefix_sum_map:
+            count += prefix_sum_map[curr_prefix_sum - k]
+            #count += 1
+        # Update the map with the current prefix sum
+        freq  = prefix_sum_map.get(curr_prefix_sum, 0)
+        prefix_sum_map[curr_prefix_sum] = freq + 1
+        #print(f"prefix_sum_map: {prefix_sum_map}")
+ 
+    return count 
+
+nums = [1, 2, -1, 1, 2]
+k = 3
+
+result = k_sum_subarrays_optimized(nums, k)
+print(f"Number of subarrays with sum {k}: {result}")
